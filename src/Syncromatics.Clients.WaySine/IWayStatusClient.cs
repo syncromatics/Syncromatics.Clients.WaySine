092deea6849c81a8910f096638ac9d0cc3b40795
Jeff Cuevas-Koch <jkoch@syncromatics.com>
2018-12-07 11:36:58 -0800

    (+semver: feature) Add WayStatus API integration

---
+++ src/Syncromatics.Clients.WaySine/IWayStatusClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Syncromatics.Clients.WaySine.Models;
+
+namespace Syncromatics.Clients.WaySine
+{
+    /// <summary>
+    /// Client to interact with the WaySine WayStatus API
+    /// </summary>
+    public interface IWayStatusClient
+    {
+        /// <summary>
+        /// Get status information for every available sign.
+        /// </summary>
+        /// <returns>A collection of all sign status updates available.</returns>
+        Task<ICollection<Sign>> GetAllSignsAsync();
+
+        /// <summary>
+        /// Get status information for a particular sign.
+        /// </summary>
+        /// <param name="id">The ID of the sign for which to query.</param>
+        /// <returns>Sign status for the given <paramref name="signId"/></returns>
+        Task<Sign> GetSignAsync(int signId);
+    }
+}
---
+++ tests/Syncromatics.Clients.WaySine.IntegrationTests/WayStatusClientTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Syncromatics.Clients.WaySine.IntegrationTests
+{
+    public class WayStatusClientTests
+    {
+        private readonly WayStatusClient _subject;
+
+        public WayStatusClientTests()
+        {
+            const string envVar = "WAYSTATUS_ROOT_URL";
+            var rootUrl = Environment.GetEnvironmentVariable(envVar);
+            rootUrl.Should()
+                .NotBeNull($"you must specify a root url as environment variable '{envVar}' to run these tests");
+
+            var clientSettings = new ClientSettings(serverRootUrl: rootUrl);
+            _subject = new WayStatusClient(clientSettings);
+        }
+
+        [Fact]
+        public async Task Can_get_all_signs()
+        {
+            var allSigns = await _subject.GetAllSignsAsync();
+
+            allSigns.Should().NotBeNull();
+            allSigns.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Can_get_single_sign()
+        {
+            const int signId = 592;
+            var singleSign = await _subject.GetSignAsync(signId);
+
+            singleSign.Should().NotBeNull();
+            singleSign.SignId.Should().Be(signId);
+        }
+    }
+}
