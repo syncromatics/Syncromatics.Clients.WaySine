f6854d08da13714cd838c5641897b80701f6572e
Jeff Cuevas-Koch <jcuevas-koch@gmvsync.com>
2018-12-11 14:52:07 -0800

    Handle full URLs in addition to root URLs

--- src/Syncromatics.Clients.WaySine/WayStatusClient.cs
+++ src/Syncromatics.Clients.WaySine/WayStatusClient.cs
@@ -10,6 +10,7 @@ namespace Syncromatics.Clients.WaySine
     public class WayStatusClient : IWayStatusClient
     {
         private readonly IWayStatusApi _api;
+        private const string UrlSuffix = "WayStatus/Api/Services.svc/GetSigns";
 
         public WayStatusClient(ClientSettings clientSettings)
         {
@@ -17,6 +18,13 @@ namespace Syncromatics.Clients.WaySine
             {
                 throw new ArgumentNullException(nameof(clientSettings));
             }
+
+            var serverRootUrl = clientSettings.ServerRootUrl;
+            if (serverRootUrl.ToLower().EndsWith(UrlSuffix.ToLower()))
+            {
+                serverRootUrl = serverRootUrl.Substring(0, serverRootUrl.Length - UrlSuffix.Length);
+            }
+
             _api = RestClient.For<IWayStatusApi>(clientSettings.ServerRootUrl);
         }
 
